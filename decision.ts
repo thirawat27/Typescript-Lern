c9edfe7c85433b010fc0bf93d4cb6fe01a296c92
thirawat27 <thunthirawas@gmail.com>
2022-11-10 09:46:17 +0700

    commit

---
+++ decision.ts
@@ -0,0 +1,36 @@
+/* decision */
+
+
+//if else if else
+
+let nuber: number = 68;
+let nuber2: number = 89;
+
+if (nuber >  nuber2) {
+    console.log("Very Good");
+}else {
+    console.log("Ho no");
+}
+
+
+if (nuber2 <= nuber) {
+    console.log("OK");
+}else if (nuber >= nuber2){
+    console.log("okkk");
+}else {
+    console.log("ON");
+}
+
+// switch case
+
+let casefun : number =  75;
+
+switch (casefun){
+   case 68:
+    console.log("ok number 1");
+   break
+   case 75:
+    console.log("ok number 2");
+   break
+}
+
