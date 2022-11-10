877004204bf50f7ebd42c73bda4d58ef56a60332
thirawat27 <thunthirawas@gmail.com>
2022-11-10 19:13:47 +0700

    commit

---
+++ loop.ts
@@ -0,0 +1,25 @@
+/** Loop */
+
+
+// for loop
+for (let i = 0; i < 3; i++) {
+    console.log ("for it is " + i);
+  }
+
+// while loop
+
+  let ip: number = 2;
+
+  while (ip < 4) {
+      console.log( "while it is " + ip )
+      ip++;
+  }
+
+//do while loop
+
+let io: number = 2;
+
+do {
+    console.log("do while it is " + io )
+    io++;
+} while ( io < 4)
